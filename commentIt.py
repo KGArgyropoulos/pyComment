5be33eb5a671c1f04f4c842582d5443bf39e0692
Konstantinos Argyropoulos <kon.argyr97@gmail.com>
2020-06-10 10:08:42 +0300

    Update commentIt.py

--- commentIt.py
+++ commentIt.py
@@ -39,10 +39,3 @@ with open(filename, 'w+') as f:
     for item in commentedText:
         f.write("%s" % item)
 f.close()
-
-#######TASKS#######
-## 1. The path should be absolute
-##	  It should also be both Linux and Windows compatible
-## 2. Error handling messages
-## 3. Clear the Code (will be github contributed)
-## 4. Test cases
